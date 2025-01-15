fc99a8c600038ff3c828591c75a22fc523a3c04a
andi-mache <amache75@gmail.com>
2025-01-15 15:07:14 +0300

    chore: cleanup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ShopInvenManage
+
 The Shop Inventory Management System is a web app built on the Django framework. It allows shop managers to add, update and delete items, categorized for easy inventory tracking. Managers can also track sales by category and item, providing valuable insights into inventory and sales management.
+
+> [!TIP]
+> add screenshots here
--- shop_inventory/urls.py
+++ shop_inventory/urls.py
@@ -1,4 +1,5 @@
 """SalesManagementSystem URL Configuration"""
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -9,32 +10,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('shop/',include('shop.urls')),
-    path('register/', user_views.register, name='register'),
-    path('dashboard/', user_views.dashboard, name='dashboard'),
-    path('update-profile/', user_views.update_profile, name='update-profile'),
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', auth_views.logout_then_login, name='logout'),
-    
-    path('user', shop_views.user),
-    path('show', shop_views.show),
-    path('edit/<int:id>', shop_views.edit),
-    path('update/<int:id>', shop_views.update),
-    path('delete/<int:id>', shop_views.delete),
-    #Items
-    path('user_items', shop_views.user_items),
-    path('show_items', shop_views.show_items),
-    path('edit_items/<int:id>', shop_views.edit_items),
-    path('update_items/<int:id>', shop_views.update_items),
-    path('delete_items/<int:id>', shop_views.delete_items),
-
-    #sales
-    path('user_catsales', shop_views.user_catsales),
-    path('show_catsales', shop_views.show_catsales),
-    path('delete_catsales/<int:id>', shop_views.delete_catsales),
+    path("admin/", admin.site.urls),
+    # path('shop/',include('shop.urls')),
+    path("register/", user_views.register, name="register"),
+    path("dashboard/", user_views.dashboard, name="dashboard"),
+    path("update-profile/", user_views.update_profile, name="update-profile"),
+    path(
+        "", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"
+    ),
+    path("logout", auth_views.logout_then_login, name="logout"),
+    path("user", shop_views.user),
+    path("show", shop_views.show),
+    path("edit/<int:id>", shop_views.edit),
+    path("update/<int:id>", shop_views.update),
+    path("delete/<int:id>", shop_views.delete),
+    # Items
+    path("user_items", shop_views.user_items),
+    path("show_items", shop_views.show_items),
+    path("edit_items/<int:id>", shop_views.edit_items),
+    path("update_items/<int:id>", shop_views.update_items),
+    path("delete_items/<int:id>", shop_views.delete_items),
+    # sales
+    path("user_catsales", shop_views.user_catsales),
+    path("show_catsales", shop_views.show_catsales),
+    path("delete_catsales/<int:id>", shop_views.delete_catsales),
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
